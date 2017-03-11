330eeff816652b1ff411e122cb0afe42950e6cdd
R cqls <rdrouilh@gmail.com>
2017-03-11 10:07:01 +0100

    Oups nothing major

--- lib/jl4rb/jl2rb_init.rb
+++ lib/jl4rb/jl2rb_init.rb
@@ -4,8 +4,9 @@ module Julia
     unless Julia.alive?
       args={:img=>File.join(ENV["JULIA_DIR"],"lib")}.merge(args) if ENV["JULIA_DIR"]
       args={:img=>ENV["JULIA_IMG_DIR"]}.merge(args) if ENV["JULIA_IMG_DIR"]
+      #p [:julia_init,args,ENV]
       @@initJL=Julia.initJL([args[:img]])
-    end 
+    end
   end
 
   def Julia.alive?
